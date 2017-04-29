e7137e91af0777fe58d37ee2c724a7571634b7f7
bailin7134 <lbai.ee.ethz@gmail.com>
2017-04-29 08:38:20 +0200

    draft assignment 4

--- assignment4/assignment4.lua
+++ assignment4/assignment4.lua
@@ -6,66 +6,34 @@ require 'optim'
 -- define labels
 local label1 = torch.rand(1,3,64,64)
 local label2 = torch.rand(1,3,64,64)
--- TODO
-local function createResBlock()
-	local resBlock = nn.Sequential()
-	-- TODO implement resblock in this function
-	-- do not implement this separately
-	return resBlock
-end
-local model = nn.ParallelTable()
-local L1Net = nn.Sequential()
-local L2Net = nn.Sequential()
-local conv1 = nn.Sequential()
-local conv4 = nn.Sequential()
--- Define conv1 and conv4 layers
-conv1:add(nn.SpatialConvolution(3, 32, 3, 3,1,1,1,1))
-conv4:add(nn.SpatialConvolution(32, 3, 3, 3,1,1,1,1))
 
--- TODO add shared conv1 layer to L1Net and L2Net
+input1=torch.rand(1,32,64,64):double()
 
--- TODO add ResBlock to L1Net and L2Net
+input = nn.Identity()()
+cat = nn.Sequential()
+cat:add(nn.SpatialConvolution(32, 32, 3, 3,1,1,1,1))
+cat:add(nn.ReLU(true))
+cat:add(nn.SpatialConvolution(32, 32, 3, 3,1,1,1,1))
+--cat:forward(input)
+--print(cat.output:size())
 
--- TODO add shared conv4 layer to L1Net and L2Net
+conBlock = nn.ConcatTable()
+conBlock:add(nn.Identity())
+conBlock:add(cat)
+conResult = conBlock:forward(input1)
 
--- TODO add L1Net and L2Net to model
+addBlock = nn.CAddTable()
+resBlock = addBlock:forward(conResult)
 
--- define criterion
-criterion1 = nn.MSECriterion()
-criterion2 = nn.AbsCriterion()
-if model then
-   parameters,gradParameters = model:getParameters()
-end
-print '==> configuring optimizer'
-optimMethod = optim.adam
-local parameters, gradParameters = model:getParameters()
-feval = function(x)
-  model:zeroGradParameters()
-  local inputs1 = torch.rand(1,3,64,64)
-  local inputs2 = torch.rand(1,3,64,64)
-  outputs = model:forward({inputs1, inputs2}) 
-  err1 = criterion1:forward(outputs[1], label1)
-  err2 = criterion2:forward(outputs[2], label2)
+--pred = conBlock:forward(input1)
+print(resBlock:size())
+--test = nn.CAddTable()(pred)
+--print(test:size())
+--for i, k in ipairs(pred) do print(i, k) end
+--output = resBlock(input)
+--mlp = nn.gModule({input,input}, output)
+--resBlock:forward(inputDouble)
+-- print(resBlock.output:size())
+--resBlock:add(nn.CAddTable(true))
+--resBlock:forward(inputDouble)
 
-  local gradOutputs1   = criterion1:backward(outputs[1], label1)
-  local gradOutputs2   = criterion2:backward(outputs[2], label2)
-
-  model:backward({inputs1, inputs2}, {gradOutputs1, gradOutputs2})
-  err = err1 + err2
-  return err, gradParameters
-end
--- run 10 iterations
-for iii=1,10 do
-	optim.adam(feval, parameters, optimState)
-	print(err)
-end
--- Test whether parameters are shared, don't remove this part
-local m1 = nn.Sequential()
-m1:add(model.modules[1].modules[1])
-local m2 = nn.Sequential()
-m2:add(model.modules[2].modules[1])
-parameters1,gradParameters1 = m1:getParameters()
-parameters2,gradParameters2 = m2:getParameters()
-local diff = parameters1 - parameters2
-print(diff:min())
-print(diff:max())
